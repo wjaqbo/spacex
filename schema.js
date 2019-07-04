f2c3121c0ec9b0be72023b4ce8f127f4a40d81ca
Wiktor Jakubowski <wjakubowski@divante.pl>
2019-07-04 13:56:44 +0200

    Add eslint to server files.

--- schema.js
+++ schema.js
@@ -1,5 +1,22 @@
-const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean, GraphQLList, GraphQLSchema } = require('graphql');
+const {
+    GraphQLObjectType,
+    GraphQLInt,
+    GraphQLString,
+    GraphQLBoolean,
+    GraphQLList,
+    GraphQLSchema,
+} = require('graphql');
 const axios = require('axios');
+
+// Rocket Type
+const RocketType = new GraphQLObjectType({
+    name: 'Rocket',
+    fields: () => ({
+        rocket_id: { type: GraphQLString },
+        rocket_name: { type: GraphQLString },
+        rocket_type: { type: GraphQLString },
+    }),
+});
 
 // Launch Type
 const LaunchType = new GraphQLObjectType({
@@ -10,21 +27,11 @@ const LaunchType = new GraphQLObjectType({
         launch_year: { type: GraphQLString },
         launch_date_local: { type: GraphQLString },
         launch_success: { type: GraphQLBoolean },
-        rocket: { type: RocketType }
-    })
-})
-
-// Rocket Type
-const RocketType = new GraphQLObjectType({
-    name: 'Rocket',
-    fields: () => ({
-        rocket_id: { type: GraphQLString },
-        rocket_name: { type: GraphQLString },
-        rocket_type: { type: GraphQLString }
-    })
+        rocket: { type: RocketType },
+    }),
 });
 
-//Root Query
+// Root Query
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
@@ -62,9 +69,9 @@ const RootQuery = new GraphQLObjectType({
                     .then(res => res.data);
             },
         },
-    }
+    },
 });
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
 });
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema');
 const cors = require('cors');
 const path = require('path');
+const schema = require('./schema');
 
 const app = express();
 
@@ -10,16 +10,19 @@ const app = express();
 
 app.use(cors());
 
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: true
-}));
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: true,
+    })
+);
 
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-})
+});
 
 const PORT = process.env.PORT || 5000;
 
